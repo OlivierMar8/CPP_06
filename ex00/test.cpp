2333928cda34b1c074bac425053b8ea590ced625
olmartin <olmartin@student.42lausanne.ch>
2022-11-15 15:14:24 +0100

    test parsing ok

--- ex00/test.cpp
+++ ex00/test.cpp
@@ -3,11 +3,49 @@
 #include <iostream>
 
 
-size_t first_non_digit(std::string str) {
-	size_t i = 0;
+size_t first_non_digit(std::string str, size_t i) {
    while (isdigit(str[i]))
 	   i++;
    return i;
+}
+
+int	testDigit( std::string str, int flag)
+{
+	size_t i = 0;
+	i = first_non_digit(str, i);
+	while ( i < str.size())
+   	{
+		if ((str[i] == '-' || str[i] == '+') && i == 0) 
+			flag += 1;
+		else if (str[i] == '.')
+			flag += 2;
+		else if (str[i] == 'f' && (i == str.size() - 1))
+		{
+			if (flag >= 2)
+				flag = 4;
+			else
+				flag = 5;
+		}
+		else
+		{
+			flag = 5;
+			break;
+		}
+		i = first_non_digit(str, i + 1);
+	}	
+	switch (flag) {
+					case 0:		std::cout << "Type is int" << std::endl;
+								break;
+					case 1:		std::cout << "Type is int" << std::endl;
+								break;
+					case 2:		std::cout << "Type is double" << std::endl;
+								break;
+					case 4:		std::cout << "Type is float" << std::endl;
+								break;
+					case 5:		std::cout << "Unknown Type" << std::endl;
+								break;
+	}	
+	return i;
 }
 
 
@@ -17,22 +55,7 @@ void searchType( std::string str, int flag) {
 	
 	size_t i = 0;
 	std::cout << "Entre dans searchType" << std::endl;
-	i = first_non_digit(str);
-	if ( i == str.npos) {
-		std::cout << "Type is int" << std::endl;
-		return;
-	}
-/*	else if (str[i] == 'f' and i = str.size() - 1) { //faux, pas de test du point !
-		std::cout << "Type is float" << std::endl;
-		return;
-	}
-*/	else if (str[i] == '-' || str[i] == '+') 
-	{
-		flag += 1;
-		if (flag == 1)
-			i = first_non_digit(str.substr(i + 1));
-			if ( i == 
-	}
+	i = testDigit(str, flag);
 
 	std::cout << i << std::endl;
 	return;
