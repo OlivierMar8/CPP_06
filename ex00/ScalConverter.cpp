8fd3a5c027917ddc7823dd3b4ae46d5e2c76945a
olmartin <olmartin@student.42lausanne.ch>
2022-11-16 17:11:14 +0100

    ex00 en cours

--- ex00/ScalConverter.cpp
+++ ex00/ScalConverter.cpp
@@ -32,7 +32,7 @@ ScalConverter::ScalConverter( std::string const str ) : _str( str ) {
 				//_dbleValue = std::strtod(_str);
 				break;
 		case 5: _Type = TypeFloat;
-				//_dbleValue = std::strtod(_str);
+				convert_float();
 				break;
 		default: _Type = TypeUnknown;
 	}
@@ -72,7 +72,7 @@ int		ScalConverter::getSpecialCases( int flag) {
 		_p_val[1] = 4;
 		_p_val[2] = 4;
 		_p_val[3] = 4;
-		flag = 0;
+		flag = 9;
 	}
 	else if (_str.size() == 1)
 	{
@@ -86,6 +86,22 @@ int		ScalConverter::getSpecialCases( int flag) {
 			flag = 0;
 		//	std::cout << "Type is char" << std::endl;
 		}
+	}
+	else if (!(_str.compare("-inff") || _str.compare("+inff") || _str.compare("nanf")))
+	{
+		flag = 5;
+		_p_val[0] = 3;
+		_p_val[1] = 3;
+		_p_val[2] = 2;
+		_p_val[3] = 2;
+	}
+	else if (!(_str.compare("-inf") || _str.compare("+inf") || _str.compare("nan")))
+	{
+		flag = 4;
+		_p_val[0] = 3;
+		_p_val[1] = 3;
+		_p_val[2] = 2;
+		_p_val[3] = 2;
 	}
 	return flag;
 }
@@ -106,7 +122,7 @@ int		ScalConverter::testDigits( int flag ) {
 				flag += 3;
 			else if (_str[i] == 'f' && ( i == _str.size() - 1 ))
 			{
-				if (flag >=3)
+				if (flag >= 2)
 					flag = 5;
 				else
 					flag = 6;
@@ -163,12 +179,63 @@ void	ScalConverter::convert_int( void ) {
 
 	return;
 }
+
+void	ScalConverter::convert_float( void ) {
+
+	double d;
+
+	std::istringstream(_str) >> d;
+	if ( d < std::numeric_limits<int>::min() ||
+		   	d > std::numeric_limits<int>::max())
+	{
+		_p_val[0] = 3;
+		_p_val[1] = 3;
+		_doubleValue = d;
+		_floatValue = static_cast<float>(d);
+	}
+	else
+	{
+		std::istringstream(_str) >> _floatValue;
+		_charValue = static_cast<char>(_floatValue);
+		_doubleValue = static_cast<double>(_floatValue);
+		_intValue = static_cast<int>(_floatValue);
+	}
+
+	return;
+}
+
+void	ScalConverter::convert_double( void ) {
+
+	double d;
+
+	std::istringstream(_str) >> d;
+	if ( d < std::numeric_limits<int>::min() ||
+		   	d > std::numeric_limits<int>::max())
+	{
+		_p_val[0] = 3;
+		_p_val[1] = 3;
+		_doubleValue = d;
+		_floatValue = static_cast<float>(d);
+	}
+	else
+	{
+		_doubleValue = d;
+		_charValue = static_cast<char>(_doubleValue);
+		_floatValue = static_cast<float>(_doubleValue);
+		_intValue = static_cast<int>(_doubleValue);
+	}
+
+	return;
+}
+
 void	ScalConverter::show_cast( void ) {
 
 	if (_Type == TypeChar)
 		display_char();
 	else if (_Type == TypeInt)
 		display_int();
+	else if (_Type == TypeFloat)
+		display_float();
 
 	return;
 }
@@ -184,6 +251,26 @@ void	ScalConverter::display_char( void ) {
 }	
 
 void	ScalConverter::display_int( void ) {
+
+	std::cout << "char: ";
+	if ((_intValue > 0 && _intValue < 32) || _intValue == 127)
+		std::cout << "Non displayable" << std::endl;
+	else if (_intValue < 1 || _intValue > 127)
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << _charValue << std::endl; 
+	std::cout << "int: "; 
+	if (_p_val[1] == 3)
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << _intValue << std::endl; 
+	std::cout << "float: " << _floatValue << "f" << std::endl; 
+	std::cout << "double: " << _doubleValue << std::endl; 
+	
+	return;
+}	
+
+void	ScalConverter::display_float( void ) {
 
 	std::cout << "char: ";
 	if ((_intValue > 0 && _intValue < 32) || _intValue == 127)
