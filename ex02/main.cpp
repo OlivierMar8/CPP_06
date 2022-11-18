f182897b4dabfbd831e4542cfc60b60e54feb548
olmartin <olmartin@student.42lausanne.ch>
2022-11-18 14:12:22 +0100

    generate OK

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,13 +1,47 @@
 //main.cpp	CPP06	ex01
 
 #include <iostream>
+#include <cstdlib>
 #include "base.hpp"
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
 
+Base * generate( void ) {
+	
+	Base * ptr;
+	int		a = std::rand() % 3 + 1;
+	switch (a)
+	{
+		case 1:	ptr = new A();
+				std::cout << " A \n";
+				break;
+		case 2:	ptr = new B();
+				std::cout << " B \n";
+				break;
+		case 3:	ptr = new C();
+				std::cout << " C \n";
+				break;
+		default: std::cout << "Default. value = " << a << " \n";
+				 ptr = new Base();
+		std::cout << std::endl;
+	}
+	return (ptr);
+}
+
 int	main( void )
 {
+	Base * pointeur;
+
+	for (int i = 0; i < 10; i++)
+	{
+		pointeur = generate();
+		delete pointeur;
+	}
+
+
+
+
 	return 0;
 }
 
