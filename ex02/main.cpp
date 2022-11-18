7f088e328dbb894dcbee905c301c54a041254519
olmartin <olmartin@student.42lausanne.ch>
2022-11-18 14:54:58 +0100

    ex02 identify * OK

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -29,13 +29,36 @@ Base * generate( void ) {
 	return (ptr);
 }
 
+void	identify( Base* p) {
+
+	A* ptrA = dynamic_cast<A*>(p);
+	if (ptrA != NULL)
+		std::cout << "The real type is A" << std::endl;
+	else
+	{
+		B* ptrB = dynamic_cast<B*>(p);
+		if (ptrB != NULL)
+			std::cout << "The real type is B" << std::endl;
+		else
+		{
+			C* ptrC = dynamic_cast<C*>(p);
+			if (ptrC != NULL)
+				std::cout << "The real type is C" << std::endl;
+			else
+				std::cout << "The real type is unknown" << std::endl;
+		}
+	}
+	return;
+}
+
 int	main( void )
 {
-	Base * pointeur;
+	Base* pointeur;
 
 	for (int i = 0; i < 10; i++)
 	{
 		pointeur = generate();
+		identify(pointeur);
 		delete pointeur;
 	}
 
